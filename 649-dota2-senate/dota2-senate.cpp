e3c2abc868ecf24f87115770993e493735ef773d
Ridhi Garg <22.ridhigarg@gmail.com>
2026-01-13 19:37:18 +0530

    Time: 5 ms (29.55%) | Memory: 11.6 MB (28.87%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> r, d;
+        int n = senate.size();
+
+        for (int i = 0; i < n; i++) {
+            if (senate[i] == 'R') r.push(i);
+            else d.push(i);
+        }
+
+        while (!r.empty() && !d.empty()) {
+            int ri = r.front(); r.pop();
+            int di = d.front(); d.pop();
+
+            if (ri < di) {
+                r.push(ri + n);   // Radiant bans Dire
+            } else {
+                d.push(di + n);   // Dire bans Radiant
+            }
+        }
+
+        return r.empty() ? "Dire" : "Radiant";
+    }
+};
